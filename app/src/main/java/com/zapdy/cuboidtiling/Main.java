32539edac07241d7a41f02e18027910b4e5571c1
= <zapdyy@gmail.com>
2026-04-18 16:05:35 +0200

    initial commit

---
+++ app/src/main/java/com/zapdy/cuboidtiling/Main.java
@@ -0,0 +1,7 @@
+package com.zapdy.cuboidtiling;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Cuboid Tiling");
+    }
+}
---
+++ app/src/test/java/com/zapdy/cuboidtiling/AppTest.java
@@ -0,0 +1,11 @@
+package com.zapdy.cuboidtiling;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test void appHasAGreeting() {
+    assertEquals(true, true);
+    }
+}
+
